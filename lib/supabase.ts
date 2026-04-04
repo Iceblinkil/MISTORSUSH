bf945ff1f9b2302a47b4c5a78c1dca1060c030e6
Michael <misha.shveynfurt@gmail.com>
2026-04-04 22:03:16 +0300

    feat: initialize Supabase client and configure environment variable templates

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const S_URL = 'https://iaqbtwsothmkdjapstkq.supabase.co';
-const S_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhcWJ0d3NvdGhta2RqYXBzdGtxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyNjE0ODYsImV4cCI6MjA4OTgzNzQ4Nn0.ye0QGkypstTfxGjCkytn_x7bwsa-2tNiq3EIEulsvV0';
+const S_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const S_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const sb = createClient(S_URL, S_KEY);
